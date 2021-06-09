2a09a42ccadd7c8498ea25c180c0bb7e9b6b62be
StijnGyssens <stijn.gyssens@gmail.com>
2021-06-09 08:31:17 +0200

    fout in api opgelost

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {knex} from "../../components/helpers"
+import {knex} from "../../components/knex"
 
 export default async (req, res) => {
   const result = await knex.select().table("events")
