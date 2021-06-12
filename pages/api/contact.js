190eefe071a87a6d9d4fb47181e78349550c3819
StijnGyssens <stijn.gyssens@gmail.com>
2021-06-12 12:48:22 +0200

    probleem met nodemailer testen

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,8 +2,9 @@ export default function (req, res) {
     
     let nodemailer = require('nodemailer')
     let transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
+      // port: 465,
+      // host: "smtp.gmail.com",
+      service:"gmail",
       auth: {
         user: 'ogllonderzeel1840@gmail.com',
         pass: process.env.MAILPASS,
